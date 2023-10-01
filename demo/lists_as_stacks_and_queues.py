36f1c06845a87a1ac433544ec65187c606c74243
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-01 16:08:04 +0300

    Add files via upload

--- demo/lists_as_stacks_and_queues.py
+++ demo/lists_as_stacks_and_queues.py
@@ -1,4 +1,4 @@
-"""
+'''
 01.Reverse Numbers
 
 def reverse_numbers(numbers):
@@ -6,4 +6,27 @@ def reverse_numbers(numbers):
 
 
 reverse_numbers([x for x in input().split(' ')])
-"""
+'''
+
+'''
+02. Stacked Queries
+
+def stacked_queries():
+    stack, integer = [], int(input())
+    for _ in range(integer):
+        query = input().split(' ')
+        if query[0] == '1':
+            stack.append(int(query[1]))
+        elif query[0] == '2' and stack:
+            stack.pop()
+        elif query[0] == '3' and stack:
+            print(max(stack))
+        elif query[0] == '4' and stack:
+            print(min(stack))
+    stack_for_return = stack[::-1]
+    print(', '.join(str(x) for x in stack_for_return))
+
+
+stacked_queries()
+'''
+
