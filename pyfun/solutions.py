d6f40aab2a6fccd2b307f298aa91e1c5d8bf5e7a
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-02 12:04:53 +0300

    Add files via upload

--- pyfun/solutions.py
+++ pyfun/solutions.py
@@ -1,4 +1,5 @@
 # 01. Pythagorean tripe
+"""
 correct_triples_count = 0
 for c in range(1, 48):
     for b in range(1, c):
@@ -7,3 +8,28 @@ for c in range(1, 48):
                 correct_triples_count += 1
                 print('{}, {}, {}'.format(a, b, c))
 print(correct_triples_count)
+"""
+
+# 02. Heap's algorithm
+import sys
+
+"""
+def generate_permutations(a, n):
+    if n == 0:
+        print(''.join(a))
+    else:
+        for i in range(n):
+            generate_permutations(a, n - 1)
+            j = 0 if n % 2 == 0 else i
+            a[j], a[n] = a[n], a[j]
+        generate_permutations(a, n - 1)
+
+
+if len(sys.argv) != 2:
+    sys.stderr.write('Exactly one argument is required\n')
+    sys.exit(1)
+
+word = sys.argv[1]
+
+generate_permutations(list(word), len(word) - 1)
+"""
