12cf7a222463c8ab0d7f1cb86aa573103f087104
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-12 06:43:38 +0300

    Add files via upload

---
+++ training_demo/basics.py
@@ -0,0 +1,16 @@
+# # 01. Tennis Equipment
+#
+# from math import floor, ceil
+#
+# racquet_price = float(input())
+# racquet_number = int(input())
+# trainers_number = int(input())
+# trainers_price = racquet_price / 6
+#
+# racquets_and_trainers_price = racquet_price * racquet_number + trainers_price * trainers_number
+# other_equipment = racquets_and_trainers_price * 0.2
+# total_price = racquets_and_trainers_price + other_equipment
+# player_price = floor(total_price / 8)
+# sponsors_price = ceil(total_price * 7 / 8)
+# print(f'Price to be paid by Djokovic {player_price}')
+# print(f'Price to be paid by sponsors {sponsors_price}')
