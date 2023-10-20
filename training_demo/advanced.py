6a71431abb611815f11938979220b1380835c94b
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-20 11:55:19 +0300

    Add files via upload

--- training_demo/advanced.py
+++ training_demo/advanced.py
@@ -1,24 +1,27 @@
+"""
+# 01. Monster Extermination
 from collections import deque
 
-armors = deque([int(x) for x in input().split(',')])
-strikes = [int(x) for x in input().split(',')]
+monsters_armors = deque([int(x) for x in input().split(',')])
+soldier_strikes = [int(x) for x in input().split(',')]
 killed_monsters = 0
 
-while armors and strikes:
-    armor, strike = armors.popleft(), strikes.pop()
+while monsters_armors and soldier_strikes:
+    armor, strike = monsters_armors.popleft(), soldier_strikes.pop()
     if strike >= armor:
         killed_monsters += 1
         strike -= armor
         if strike != 0:
-            if strikes:
-                strikes[-1] += strike
+            if soldier_strikes:
+                soldier_strikes[-1] += strike
             else:
-                strikes.append(strike)
+                soldier_strikes.append(strike)
     else:
         armor -= strike
-        armors.append(armor)
-if not armors:
+        monsters_armors.append(armor)
+if not monsters_armors:
     print('All monsters have been killed!')
-if not strikes:
+if not soldier_strikes:
     print('The soldier has been defeated.')
 print(f'Total monsters killed: {killed_monsters}')
+"""
