c301f41525ee29e28a7561b5a47a5a03a7ede71e
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-22 13:46:50 +0300

    Add files via upload

--- training_demo/oop_handball/project/teams/base_team.py
+++ training_demo/oop_handball/project/teams/base_team.py
@@ -15,8 +15,24 @@ class BaseTeam(ABC):
     def win(self):
         ...
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if value.strip() == ' ':
+            raise ValueError('Team name cannot be empty!')
+        self._name = value
+
     def get_statistics(self):
-        self.budget = f'{self.budget:.2f}'
-        total_equipment_price = f'{sum([el.price for el in self.equipment]):.2f}'
+        total_equipment_price = sum([el.price for el in self.equipment])
         protections = [el.protection for el in self.equipment]
         avg_protection = floor(sum(protections) / len(protections))
+        return (f'Name: {self.name}\n'
+                f'Country: {self.country}\n'
+                f'Advantage: {self.advantage} points\n'
+                f'Budget: {self.budget:.2f}EUR\n'
+                f'Wins: {self.wins}\n'
+                f'Total Equipment Price: {total_equipment_price:.2f}\n'
+                f'Average Protection: {floor(avg_protection)}')
