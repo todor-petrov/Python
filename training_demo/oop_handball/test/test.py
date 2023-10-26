cd72276fb9f1b3d71e2283616e9189c82a2a2b4f
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-26 12:23:17 +0300

    Add files via upload

--- training_demo/oop_handball/test/test.py
+++ training_demo/oop_handball/test/test.py
@@ -4,7 +4,13 @@ from project.trip import Trip
 
 class TripTester(TestCase):
     def setUp(self):
-        ...
+        self.trip = Trip(10000.0, 3, True)
+
+    def test_initialization(self):
+        self.assertEqual(self.trip.budget, 10000.0)
+        self.assertEqual(self.trip.travelers, 3)
+        self.assertEqual(self.trip.is_family, True)
+        self.assertEqual(self.trip.booked_destinations_paid_amounts, {})
 
 
 if 'name' == '__main__':
