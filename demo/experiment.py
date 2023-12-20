e0aae8af9ba566ac00fbf1c396a1b5ffe9047c9b
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-12-20 08:02:23 +0200

    Add files via upload

---
+++ demo/experiment.py
@@ -0,0 +1,28 @@
+# class Book:
+#     def __init__(self, name, author, pages):
+#         self.name = name
+#         self.author = author
+#         self.pages = pages
+
+
+# class Car:
+#     def __init__(self, name, model, engine):
+#         self.name = name
+#         self.model = model
+#         self.engine = engine
+
+#     def get_info(self):
+#         return f'This is {self.name} {self.model} with engine {self.engine}'
+
+
+# class Music:
+#     def __init__(self, title, artist, lyrics):
+#         self.title = title
+#         self.artist = artist
+#         self.lyrics = lyrics
+
+#     def print_info(self):
+#         return f'This is "{self.title}" from "{self.artist}"'
+    
+#     def play(self):
+#         return self.lyrics
