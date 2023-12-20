60f909355fc3f93e336fd3794aab0462ccf737df
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-12-20 12:08:31 +0200

    Add files via upload

--- demo/experiment.py
+++ demo/experiment.py
@@ -70,3 +70,34 @@
 #         return self.salary
 
 
+# class Cup:
+#     def __init__(self, size, quantity):
+#          self.size = size
+#          self.quantity = quantity
+
+#     def fill(self, quantity):
+#         if self.quantity + quantity <= self.size:
+#             self.quantity += quantity
+
+#     def status(self):
+#         return self.size - self.quantity
+    
+
+# class Flower:
+#     def __init__(self, name, water_requirements):
+#         self.name = name
+#         self.water_requirements = water_requirements
+#         self.is_happy = False
+        
+#     def water(self, quantity):
+#         if quantity >= self.water_requirements:
+#             self.is_happy = True
+#             return
+#         self.is_happy = False
+        
+#     def status(self):
+#         if self.is_happy:
+#             return f'{self.name} is happy'
+#         return f'{self.name} is not happy'
+    
+
