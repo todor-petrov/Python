0a509263a9ff4b14a18e0ab6e7d17ebf6cc800d5
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-12-22 17:15:59 +0200

    Add files via upload

--- demo/classes_objects.py
+++ demo/classes_objects.py
@@ -78,44 +78,118 @@
 #         return f'Total apps: {len(self.apps)}. Memory left: {self.memory}'
 
 
-class Vet:
+# class Vet:
+#
+#     animals = []
+#     space = 5
+#
+#     def __init__(self, name):
+#         self.name = name
+#         self.animals = []
+#
+#     def register_animal(self, animal_name):
+#         if len(Vet.animals) < Vet.space:
+#             self.animals.append(animal_name)
+#             Vet.animals.append(animal_name)
+#             return f'{animal_name} registered in the clinic'
+#         return 'Not enough space'
+#
+#     def unregister_animal(self, animal_name):
+#         if animal_name in Vet.animals:
+#             Vet.animals.remove(animal_name)
+#             self.animals.remove(animal_name)
+#             return f'{animal_name} unregistered successfully'
+#         return f'{animal_name} not in the clinic'
+#
+#     def info(self):
+#         space_left_in_clinic = Vet.space - len(Vet.animals)
+#         return f'{self.name} has {len(self.animals)} animals. {space_left_in_clinic} space left in clinic'
 
-    animals = []
-    space = 5
 
-    def __init__(self, name):
-        self.name = name
-        self.animals = []
+# class Time:
+#
+#     max_hours = 23
+#     max_minutes = 59
+#     max_seconds = 59
+#
+#     def __init__(self, hours, minutes, seconds):
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+#
+#     def set_time(self, hours, minutes, seconds):
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+#
+#     def get_time(self):
+#         return f'{self.hours:02}:{self.minutes:02}:{self.seconds:02}'
+#
+#     def next_second(self):
+#
+#         self.seconds += 1
+#         if self.seconds > Time.max_seconds:
+#             self.seconds = 0
+#             self.minutes += 1
+#             if self.minutes > Time.max_minutes:
+#                 self.minutes = 0
+#                 self.hours += 1
+#                 if self.hours > Time.max_hours:
+#                     self.hours = 0
+#         return self.get_time()
 
-    def register_animal(self, animal_name):
-        if len(Vet.animals) < Vet.space:
-            self.animals.append(animal_name)
-            Vet.animals.append(animal_name)
-            return f'{animal_name} registered in the clinic'
-        return 'Not enough space'
 
-    def unregister_animal(self, animal_name):
-        if animal_name in Vet.animals:
-            Vet.animals.remove(animal_name)
-            self.animals.remove(animal_name)
-            return f'{animal_name} unregistered successfully'
-        return f'{animal_name} not in the clinic'
+# class Account:
+#     def __init__(self, user_id, name, balance=0):
+#         self.id = user_id
+#         self.name = name
+#         self.balance = balance
+#
+#     def credit(self, amount):
+#         self.balance += amount
+#         return self.balance
+#
+#     def debit(self, amount):
+#         if amount <= self.balance:
+#             self.balance -= amount
+#             return self.balance
+#         return 'Amount exceeded balance'
+#
+#     def info(self):
+#         return f'User {self.name} with account {self.id} has {self.balance} balance'
 
-    def info(self):
-        space_left_in_clinic = Vet.space - len(Vet.animals)
-        return f'{self.name} has {len(self.animals)} animals. {space_left_in_clinic} space left in clinic'
 
+# class PizzaDelivery:
+#
+#     def __init__(self, name: str, price: float, ingredients: dict):
+#         self.name = name
+#         self.price = price
+#         self.ingredients = ingredients
+#         self.ordered = False
+#
+#     def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float):
+#         if self.ordered:
+#             return f'Pizza {self.name} already prepared, and we can\'t make any changes!'
+#         if ingredient not in self.ingredients:
+#             self.ingredients[ingredient] = 0
+#         self.ingredients[ingredient] += quantity
+#         self.price += quantity * price_per_quantity
+#
+#     def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float):
+#         if self.ordered:
+#             return f'Pizza {self.name} already prepared, and we can\'t make any changes!'
+#         if ingredient not in self.ingredients:
+#             return f'Wrong ingredient selected! We do not use {ingredient} in {self.name}!'
+#         if self.ingredients[ingredient] < quantity:
+#             return f'Please check again the desired quantity of {ingredient}!'
+#         self.ingredients[ingredient] -= quantity
+#         self.price -= quantity * price_per_quantity
+#
+#     def make_order(self):
+#         self.ordered = True
+#         ordered_ingredients = []
+#         for k, v in self.ingredients.items():
+#             ordered_ingredients.append(f'{k}: {v}')
+#         return (f"You've ordered pizza {self.name} prepared with {', '.join(ordered_ingredients)} "
+#                 f"and the price will be {self.price}lv.")
 
-peter = Vet("Peter")
-george = Vet("George")
-print(peter.register_animal("Tom"))
-print(george.register_animal("Cory"))
-print(peter.register_animal("Fishy"))
-print(peter.register_animal("Bobby"))
-print(george.register_animal("Kay"))
-print(george.unregister_animal("Cory"))
-print(peter.register_animal("Silky"))
-print(peter.unregister_animal("Molly"))
-print(peter.unregister_animal("Tom"))
-print(peter.info())
-print(george.info())
