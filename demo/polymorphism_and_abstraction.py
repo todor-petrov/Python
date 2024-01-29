f2aca4b3a934536b14df1803c8f13b398a0c9df3
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-01-29 17:12:08 +0200

    Add files via upload

---
+++ demo/polymorphism_and_abstraction.py
@@ -0,0 +1,77 @@
+# 02. ImageArea
+"""
+class ImageArea:
+
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def __gt__(self, other):
+        return self.get_area() > other.get_area()
+
+    def __ge__(self, other):
+        return self.get_area() >= other.get_area()
+
+    def __lt__(self, other):
+        return self.get_area() < other.get_area()
+
+    def __le__(self, other):
+        return self.get_area() <= other.get_area()
+
+    def __eq__(self, other):
+        return self.get_area() == other.get_area()
+
+    def __ne__(self, other):
+        return self.get_area() != other.get_area()
+"""
+
+# 03. Playing
+"""
+def start_playing(instance):
+    return instance.play()
+"""
+
+# 04. Shapes
+"""
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def calculate_area(self):
+        ...
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        ...
+
+
+class Circle(Shape):
+    
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def calculate_area(self):
+        return pi * self.__radius ** 2
+
+    def calculate_perimeter(self):
+        return 2 * pi * self.__radius
+
+
+class Rectangle(Shape):
+
+    def __init__(self, height, width):
+        self.__height = height
+        self.__width = width
+
+    def calculate_area(self):
+        return self.__height * self.__width
+
+    def calculate_perimeter(self):
+        return 2 * (self.__height + self.__width)
+"""
