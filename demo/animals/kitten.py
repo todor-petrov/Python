05eca57b2fab206a7df155ac689dbd4c42c3e0b1
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-01-31 09:04:54 +0200

    Add files via upload

---
+++ demo/animals/kitten.py
@@ -0,0 +1,10 @@
+from animals.cat import Cat
+
+
+class Kitten(Cat):
+
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age, gender='Female')
+
+    def make_sound(self):
+        return 'Meow'
---
+++ demo/animals/tomcat.py
@@ -0,0 +1,10 @@
+from animals.cat import Cat
+
+
+class Tomcat(Cat):
+
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age, gender='Male')
+
+    def make_sound(self):
+        return 'Hiss'
