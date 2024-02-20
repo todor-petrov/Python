bf909ee884735fcbc9bcb031cb5c3edb20dcc7c7
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-02-20 16:34:23 +0200

    Add files via upload

---
+++ rent_a_car/user.py
@@ -0,0 +1,72 @@
+from project import validator
+
+
+class User:
+
+    INCREASING_RATING = 0.5
+    MAX_RATING = 10.0
+    DECREASING_RATING = 2.0
+    MIN_RATING = 0.0
+
+    def __init__(self, first_name: str, last_name: str, driving_license_number: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.driving_license_number = driving_license_number
+        self.rating = 0.0
+        self.is_blocked = False
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        if not value.strip():
+            raise ValueError('First name cannot be empty!')
+        self.__first_name = value
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        if not value.strip():
+            raise ValueError('Last name cannot be empty!')
+        self.__last_name = value
+
+    @property
+    def driving_license_number(self):
+        return self.__driving_license_number
+
+    @driving_license_number.setter
+    def driving_license_number(self, value):
+        if not value.strip():
+            raise ValueError('Driving license number is required!')
+        self.__driving_license_number = value
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, value):
+        if value < 0:
+            raise ValueError('Users rating cannot be negative!')
+        self.__rating = value
+
+    def increase_rating(self):
+        self.rating += User.INCREASING_RATING
+        if User.MAX_RATING < self.rating:
+            self.rating = User.MAX_RATING
+
+    def decrease_rating(self):
+        self.rating -= User.DECREASING_RATING
+        if User.MIN_RATING < self.rating:
+            self.rating = User.MIN_RATING
+            self.is_blocked = True
+
+    def __str__(self):
+        return (f'{self.first_name} {self.last_name} '
+                f'Driving license: {self.driving_license_number} '
+                f'Rating: {self.rating}')
