908f650e94f51450e0550894e29531fce590189b
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-02-21 06:23:41 +0200

    Add files via upload

--- rent_a_car/managing_app.py
+++ rent_a_car/managing_app.py
@@ -1,7 +1,11 @@
 from project.user import User
+from project.vehicles.cargo_van import CargoVan
+from project.vehicles.passenger_car import PassengerCar
 
 
 class ManagingApp:
+
+    VEHICLES_TYPES = {'PassengerCar': PassengerCar, 'CargoVan': CargoVan}
 
     def __init__(self):
         self.users = []
@@ -17,7 +21,13 @@ class ManagingApp:
         return f'{first_name} {last_name} was successfully registered under DLN-{driving_license_number}'
 
     def upload_vehicle(self, vehicle_type: str, brand: str, model: str, license_plate_number: str):
-        ...
+        if vehicle_type not in ManagingApp.VEHICLES_TYPES:
+            return f'Vehicle type {vehicle_type} is inaccessible.'
+        if self.find_vehicle_by_license_plate_number(license_plate_number) is not None:
+            return f'{license_plate_number} belongs to another vehicle.'
+        vehicle = ManagingApp.VEHICLES_TYPES[vehicle_type](brand, model, license_plate_number)
+        self.vehicles.append(vehicle)
+        return f'{brand} {model} was successfully uploaded with LPN-{license_plate_number}.'
 
     def allow_route(self, start_point: str, end_point: str, length: float):
         ...
@@ -34,3 +44,7 @@ class ManagingApp:
     def find_user_by_driving_license(self, driving_license_number):
         user = [u for u in self.users if u.driving_license_number == driving_license_number]
         return user[0] if user else None
+
+    def find_vehicle_by_license_plate_number(self, license_plate_number):
+        vehicle = [v for v in self.vehicles if v.license_plate_number == license_plate_number]
+        return vehicle[0] if vehicle else None
