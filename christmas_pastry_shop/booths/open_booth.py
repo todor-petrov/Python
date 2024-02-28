ee626032dcf793533c6cdf298a3c3df8a02b0099
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-02-28 20:31:10 +0200

    Add files via upload

---
+++ christmas_pastry_shop/booths/open_booth.py
@@ -0,0 +1,10 @@
+from christmas_pastry_shop.booths.booth import Booth
+
+
+class OpenBooth(Booth):
+
+    PRICE_PER_PERSON = 2.50
+
+    def reserve(self, number_of_people: int):
+        self.price_for_reservation = OpenBooth.PRICE_PER_PERSON * number_of_people
+        self.is_reserved = True
