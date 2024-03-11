2026f214ec8503aecde133e7049346e70eb9b53d
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-03-11 06:24:25 +0200

    ...

--- Horse-Racing/01-Structure/Solution/project/horse_specification/thoroughbred.py
+++
@@ -1,9 +0,0 @@
-from project.horse_specification.horse import Horse
-
-
-class Thoroughbred(Horse):
-    MAX_SPEED = 140
-    INCREMENT_BY_TRAINING = 3
-
-    def __init__(self, name: str, speed: int):
-        super().__init__(name, speed)
