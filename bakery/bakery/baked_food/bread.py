8b28d839fb0a283098b1eed0c2cf35ec74b9ffb4
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-03-26 06:17:30 +0200

    ...

--- bakery/bakery/baked_food/bread.py
+++
@@ -1,9 +0,0 @@
-from bakery.baked_food.baked_food import BakedFood
-
-
-class Bread(BakedFood):
-
-    PORTION = 200.0
-
-    def __init__(self, name: str, price: float, portion: float = PORTION):
-        super().__init__(name, portion, price)
--- bakery/bakery/drink/tea.py
+++
@@ -1,9 +0,0 @@
-from bakery.drink.drink import Drink
-
-
-class Tea(Drink):
-
-    PRICE = 2.50
-
-    def __init__(self, name: str, portion: float, brand: str, price: float = PRICE):
-        super().__init__(name, portion, price, brand)
