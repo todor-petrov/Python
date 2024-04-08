75c57efd2d4b5914bd82a36b443a110df351ee70
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-04-08 14:26:15 +0300

    Add files via upload

---
+++ everland/people/child.py
@@ -0,0 +1,9 @@
+class Child:
+
+    DAYS_IN_MONTH = 30
+
+    def __init__(self, food_cost: int, *toys_cost):
+        self.cost = food_cost + sum(toys_cost)
+
+    def get_monthly_expense(self):
+        return self.cost * Child.DAYS_IN_MONTH
