9489b22d76814410a9ec0383e9cd5039058da9af
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-05-09 17:11:05 +0300

    Add files via upload

---
+++ medieval_games/supply/drink.py
@@ -0,0 +1,12 @@
+from medieval_games.supply.supply import Supply
+
+
+class Drink(Supply):
+
+    ENERGY = 15
+
+    def __init__(self, name: str):
+        super().__init__(name, energy=Drink.ENERGY)
+
+    def details(self):
+        return f'{self.__class__.__name__}: {self.name}, {self.energy}'
