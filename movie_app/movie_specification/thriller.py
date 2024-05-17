3b2936021af342edc2962bcaf8ce9b6c9ae7e4bb
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-05-17 17:43:42 +0300

    Add files via upload

---
+++ movie_app/movie_specification/thriller.py
@@ -0,0 +1,22 @@
+from movie_app.movie_specification.movie import Movie
+
+
+class Thriller(Movie):
+
+    def __init__(self, title, year, owner, age_restriction=16):
+        super().__init__(title, year, owner, age_restriction)
+
+    @property
+    def age_restriction(self):
+        return self.__age_restriction
+
+    @age_restriction.setter
+    def age_restriction(self, value):
+        if value < 16:
+            raise ValueError('Thriller movies must be restricted for audience under 16 years!')
+        self.__age_restriction = value
+
+    def details(self):
+        return (f'Thriller - Title:{self.title}, Year:{self.year}, '
+                f'Age restriction:{self.age_restriction}, Likes:{self.likes}, '
+                f'Owned by:{self.owner.username}')
